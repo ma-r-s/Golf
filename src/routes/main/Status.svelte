501447d94940904361fcda5e14397158c4ebffc5
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2022-09-23 15:14:38 -0500

    Fixed reconection

--- src/routes/main/Status.svelte
+++ src/routes/main/Status.svelte
@@ -8,7 +8,7 @@
 	});
 	ros.on("error", () => {
 		status = "Error";
-		color = "bg-oragne-500";
+		color = "bg-orange-500";
 		reconect();
 	});
 	ros.on("close", () => {
@@ -19,12 +19,13 @@
 
 	let reconect = () => {
 		if (status != "Conectado") {
-			ros = new ROSLIB.Ros({
-				url: "ws://localhost:9090",
-			});
-			setTimeout(reconect, 1000);
+			ros.connect("ws://localhost:9090");
+			setTimeout(reconect, 500);
 		}
+		force();
 	};
 </script>
 
-<div class="w-40 rounded-full {color} py-2 text-center text-xl font-bold  text-white shadow-lg">{status}</div>
+<div class="overflow-hidden">
+	<div class="h-6 w-6 animate-pulse rounded-full transition-all  hover:animate-none {color} text-clip py-2 text-center text-xl font-bold text-transparent shadow-lg hover:h-11 hover:w-40 hover:text-white hover:ease-in-out">{status}</div>
+</div>
