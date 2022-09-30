21d0cc37b05669b729c6dcb31a04153a112258b3
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2022-09-30 02:23:15 -0500

    working lidar threlte

--- src/lib/newPointCloud.svelte
+++ src/lib/newPointCloud.svelte
@@ -4,10 +4,10 @@
 	import * as THRELTE from '@threlte/core';
 
 	export let ros;
-	export let topicName = '/velodyne_ points';
+	export let topicName = '/velodyne_points';
 
 	let geometry = new THREE.BufferGeometry();
-	const material = new THREE.PointsMaterial({ size: 0.005, color: 0x00aaff });
+	const material = new THREE.PointsMaterial({ size: 0.005 });
 	let points = new THREE.Points(geometry, material);
 
 	//eslint-disable-next-line
@@ -21,11 +21,34 @@
 		processMessage(msg);
 	});
 
-	let flag = true;
-	let processMessage = (cloud) => {
-		if (flag) {
-			console.log(cloud);
+	function base64ToArrayBuffer(base64) {
+		var binary_string = window.atob(base64);
+		var len = binary_string.length;
+		var bytes = new Uint8Array(len);
+		for (var i = 0; i < len; i++) {
+			bytes[i] = binary_string.charCodeAt(i);
 		}
+		return bytes.buffer;
+	}
+
+	let processMessage = (cloud) => {
+		const buffer = base64ToArrayBuffer(cloud.data);
+		let vertices = [];
+		let colors = [];
+		const color = new THREE.Color(0xffffff);
+		for (let i = 0; i <= buffer.byteLength - 22; i += cloud.point_step) {
+			const data = new DataView(buffer);
+			vertices.push(
+				data.getFloat32(i, true),
+				data.getFloat32(i + 8, true),
+				data.getFloat32(i + 4, true)
+			);
+			color.setHSL((data.getFloat32(i + 12, true) * 0.66) / 2147483647, 1, 0.5);
+			color.toArray(colors, i * 3);
+		}
+		geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+		geometry.setAttribute('customColor', new THREE.Float32BufferAttribute(colors, 3));
+		points = points;
 	};
 </script>
 
--- src/routes/main/LiveLIDAR.svelte
+++ src/routes/main/LiveLIDAR.svelte
@@ -1,15 +1,20 @@
 <script>
-	import * as THREE from 'three';
 	import * as THRELTE from '@threlte/core';
+	import * as THREE from 'three';
 	import PointCloud2 from '$lib/newPointCloud.svelte';
 	export let ros;
 </script>
 
-<div class="relative h-full  w-full">
-	<THRELTE.Canvas>
-		<PointCloud2 topicName={'/points_raw'} {ros} />
+<div class="relative h-screen w-full  bg-black">
+	<THRELTE.Canvas h>
+		<PointCloud2 {ros} />
+		<THRELTE.Mesh
+			receiveShadow
+			rotation={{ x: -90 * (Math.PI / 180) }}
+			geometry={new THREE.CircleBufferGeometry(3, 72)}
+			material={new THREE.MeshStandardMaterial({ color: 'white' })}
+		/>
 		<THRELTE.AmbientLight color={0xffffff} intensity={0.3} />
-
 		<THRELTE.PerspectiveCamera position={{ x: -3, y: 4, z: 6 }}>
 			<THRELTE.OrbitControls enableZoom={true} />
 		</THRELTE.PerspectiveCamera>
