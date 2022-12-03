63117b151e933a5566b43c3656bd0be3c2f7852e
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2022-12-03 15:30:27 -0600

    Mapa file

--- src/routes/main/Destino.svelte
+++ src/routes/main/Destino.svelte
@@ -1,22 +1,17 @@
 <script>
 	import 'roslib/build/roslib';
+	export let estado;
 	let status = 'Nuevo Destino';
 	let color = 'bg-purple-500';
-	let record = () => {
-		if (status == 'Grabar') {
-			status = 'Detener';
-			color = 'bg-purple-500';
-		} else {
-			status = 'Grabar';
-			color = 'bg-sky-500';
-		}
+	let destino = () => {
+		estado = 'mapa';
 	};
 </script>
 
 <div class="overflow-hidden">
 	<button
-		on:click={record}
-		class=" rounded-full  {color} h-11 w-80 py-2 text-center text-xl font-bold text-white shadow-lg ease-in-out hover:bg-sky-600"
+		on:click={destino}
+		class=" rounded-full  {color} h-11 w-72 py-2 text-center text-xl font-bold text-white shadow-lg ease-in-out hover:bg-sky-600"
 	>
 		{status}
 	</button>
--- src/routes/main/Main.svelte
+++ src/routes/main/Main.svelte
@@ -5,26 +5,33 @@
 	import Record from './Record.svelte';
 	import Controls from './Controls.svelte';
 	import Destino from './Destino.svelte';
+	import Mapa from './mapa/Mapa.svelte';
 	// import Menu from './Menu.svelte';
 	//eslint-disable-next-line
 	let ros = new ROSLIB.Ros({
 		url: 'ws://localhost:9090'
 	});
+	let estado = 'lidar';
 </script>
 
-<div>
-	<!-- <Menu /> -->
-	<LiveLidar {ros} />
-	<div class="absolute bottom-0 right-0 m-3">
-		<Status bind:ros />
+{#if estado}
+	<div>
+		<LiveLidar {ros} />
+		<div class="absolute bottom-0 right-0 m-3">
+			<Status bind:ros />
+		</div>
+		<div class="absolute bottom-0 left-0 m-3">
+			<Record bind:ros />
+		</div>
+		<div class="absolute bottom-0 left-40 m-3">
+			<Controls bind:ros />
+		</div>
+		<div class="absolute top-0 left-0 m-3">
+			<Destino bind:estado />
+		</div>
 	</div>
-	<div class="absolute bottom-0 left-0 m-3">
-		<Record bind:ros />
+{:else}
+	<div>
+		<Mapa bind:estado />
 	</div>
-	<div class="absolute bottom-0 left-40 m-3">
-		<Controls bind:ros />
-	</div>
-	<div class="absolute top-0 left-0 m-3">
-		<Destino bind:ros />
-	</div>
-</div>
+{/if}
