27730e622cae8bd670e455cd692ffd630abfa5b1
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2022-12-03 15:52:33 -0600

    Mapa dev

---
+++ src/routes/main/mapa/ButtonLIDAR.svelte
@@ -0,0 +1,18 @@
+<script>
+	import 'roslib/build/roslib';
+	export let estado;
+	let status = 'Nuevo Destino';
+	let color = 'bg-purple-500';
+	let destino = () => {
+		estado = 'mapa';
+	};
+</script>
+
+<div class="overflow-hidden">
+	<button
+		on:click={destino}
+		class=" rounded-full  {color} h-11 w-72 py-2 text-center text-xl font-bold text-white shadow-lg ease-in-out hover:bg-sky-600"
+	>
+		{status}
+	</button>
+</div>
---
+++ src/routes/main/mapa/Mapa.svelte
@@ -0,0 +1,11 @@
+<script>
+	import LeafletMap from '$lib/LeafletMap.svelte';
+	import ButtonLIDAR from './ButtonLIDAR.svelte';
+</script>
+
+<main>
+	<div class="absolute top-0 right-0 m-3">
+		<ButtonLIDAR />
+	</div>
+	<LeafletMap />
+</main>
